9d0da091a8902d23a06f5bb2357241bac5aa94a8
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-05-20 21:03:08 +0600

    create function for creating new user with email and password

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,17 +1,23 @@
 import { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from "../firebase/firebase.confige";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    // create a new user function with email and password
+    const createNewUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
     const authInfo = {
         user,
-        loading
+        loading,
+        createNewUser,
     }
 
     return (
